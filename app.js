90d332ca9efdb28465efa88827438c59257f7232
sumi17code <sumiakter17@gmail.com>
2025-09-13 21:01:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+// Re-cape the basics of js
+
+// Variables
+
+var name = "Sumi Akter";
+                                // with var we can re-declare and re-assign the variables.
+var name = "Sultana";
+
+
+let age = 22;
+// let age =23; cannot re-declare with let but can re-assign
+
+age = 23;  
+
+// re-assigning is possible.
+
+
+const country = "Bangladesh";
+// const country = "South Korea";
+
+// can not re-declare and cannot re-assign with const.
+// so country = "south korea" will give error.
+
+
+
+// Conditions in JS
+
+const num = 20;
+
+if(num >= 15 && num <= 25){
+    console.log ("yes, the number is between 15 and 25");
+}
+else
+
+
+
