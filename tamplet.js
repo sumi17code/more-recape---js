bf50a8296dd4cf0d723e30a135adcae08f748a08
sumi17code <sumiakter17@gmail.com>
2025-09-14 11:33:48 +0600

    another js file added

---
+++ tamplet.js
@@ -0,0 +1,18 @@
+
+// ``  = template string
+
+const firstName = "Sumi";
+const lastName = "Akter";
+const hobby = "coding";
+
+const a = 30;
+const b = 20;
+
+console.log(`My name is ${firstName} ${lastName}. 
+    I love to code.
+    My hobby is ${hobby}.
+    I want to be a web developer.
+    I have ${a + b} taka in my pocket.`);
+
+    // this is a multi line string. And also we can use variable in this string. With `` (template string) we can do this dynamically.
+    // we can do any mathematical operation in this string. And also can do many more things including function and loop etc.
